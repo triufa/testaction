0cd65a23c782ba5a5f00c61eed66593c3bf52de5
sbarsocchini <sbarsocchini@shiftlabor.com>
2022-05-10 17:46:28 +0100

    test ci usuari cmmit direco

--- ConsolaNet/ConsolaNet/Program.cs
+++ ConsolaNet/ConsolaNet/Program.cs
@@ -7,7 +7,7 @@ namespace ConsolaNet
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.ReadLine();
+            Console.ReadLine()
             var f = 0;
             //dhdhd
         }
